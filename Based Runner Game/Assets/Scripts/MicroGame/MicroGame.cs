2a1492aee05138950edf8b66345763c19ed02815
N-a-n-0 <84410640+N-a-n-0@users.noreply.github.com>
2024-06-25 04:23:34 -0700

    First MicroGame (WIP)
    Work in progress math MircoMiniGame we have a functioning game, but at the moment it does not happen at the intended design. aka when we hit the MicroGame Test Obj.
    
    Scripts Changed:
    -Micro Game (Bug Fixes)
    -MicroMath
    
    GameObject we work with mostly is called MiniGameUI_Holder
    
    *NOTE*
    Found a bug - I managed to trigger the Cutscene we made with the MicroGame Prefab object while in PowerUp mode and I believe IEnumerator overriding is happening because of the fact that The PowerUp Mode messes with Speed as well in the same way the MircoGame script does.

--- Based Runner Game/Assets/Scripts/MicroGame/MicroGame.cs
+++ Based Runner Game/Assets/Scripts/MicroGame/MicroGame.cs
@@ -13,30 +13,49 @@ public class MicroGame : MonoBehaviour
     public Camera MainCamera;
 
     public Camera MicroGameCamera;
+
+    public bool alreadyEntered = false;// Used this bool to fix the issues we were having with speed
+
+    public static bool StartMiniGame = false; //I may want to use this throughout many scripts where the activation of microgames are dependent on this static bool var
+
+    public static float timeNeeded = 3f; //I wanna possibly give each microminigame its own custom timer using this var that we can just reference and change 
+
     void Update()
     {
         curSpeed = PlayerController.forwardSpeed;
-        Debug.Log("CURSPEED " + curSpeed);
+      //  Debug.Log("CURSPEED " + curSpeed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        savedSpeed = curSpeed;
-        if (other.CompareTag("Player"))
+
+        //The reason you had that speed error is because you had PlayerController.forwardSpeed = 0; outside of the if statement and you didn't have any conditions stopping the player from retriggering the OnTriggerEnter
+
+        if (other.CompareTag("Player") && alreadyEntered == false)
         {
+            print("Entered this minigame");
+            StartMiniGame = true;
+            //Make sure you can't retrigger this if statement using this bool
+            alreadyEntered = true;
+
             MainCamera.enabled = false;
             MicroGameCamera.enabled = true;
             Debug.Log("Collision: CURSPEED " + curSpeed);
             animator.SetBool("Touched", true);
-        }
-        PlayerController.forwardSpeed = 0;
 
-        StartCoroutine(returnSpeedPlz());
+
+            //moved these  lines right here in the if statement 
+            savedSpeed = curSpeed;
+            PlayerController.forwardSpeed = 0;
+
+            StartCoroutine(returnSpeedPlz());
+        }
+       
     }
 
     private IEnumerator returnSpeedPlz()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(timeNeeded);
         MainCamera.enabled = true;
         MicroGameCamera.enabled = false;
         PlayerController.forwardSpeed = savedSpeed;
---
+++ Based Runner Game/Assets/Scripts/MicroGame/MicroMath.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+//using static System.Net.Mime.MediaTypeNames;
+
+
+
+public class MicroMath : MonoBehaviour
+{
+
+    public TMP_Text ProblemText;
+
+  //  public Button Answer_1;
+   // public Button Answer_2;
+  //  public Button Answer_3;
+
+
+
+
+    public Button[] Answers;
+ 
+    private TMP_Text buttonText_1;
+    private TMP_Text buttonText_2;
+    private TMP_Text buttonText_3;
+
+    void Start()
+    {
+
+        buttonText_1 = Answers[0].GetComponentInChildren<TMP_Text>();
+        buttonText_2 = Answers[1].GetComponentInChildren<TMP_Text>();
+        buttonText_3 = Answers[2].GetComponentInChildren<TMP_Text>();
+      //  GenerateAnswer();
+
+       
+
+    }
+
+
+    public void GenerateAnswer()
+    {
+        MicroGame.timeNeeded = 5f;
+        int x = Random.Range(1, 10);
+        int y = Random.Range(1, 10);
+
+        int answer = x + y;
+
+        ProblemText.text = x + " + " + y;
+
+        print(x + " + " + y
+             + "Answer is: " + answer);
+        int correctPick = UnityEngine.Random.Range(0, 2) + 1;
+        print("Correct pick button number is:" + correctPick);
+        // Answers[correctPick].onClick.AddListener(CorrectPick);
+
+      //  buttonText = myButton.GetComponentInChildren<Text>();
+        //Continue this test micro minigame 
+
+        //Figure out how to make the answer buttons randoms and not static everytime 
+        if (correctPick == 0) 
+        {
+            Answers[correctPick].onClick.AddListener(CorrectPick); //Left button is the answer
+            buttonText_1.text = answer.ToString();
+
+             Answers[1].onClick.AddListener(IncorrectPick);
+            buttonText_2.text = (answer + UnityEngine.Random.Range(1, 3)).ToString();
+
+            Answers[2].onClick.AddListener(IncorrectPick);
+            buttonText_3.text = (answer + UnityEngine.Random.Range(4, 5)).ToString();
+
+        }
+        else if(correctPick == 1)
+        {
+            Answers[correctPick].onClick.AddListener(CorrectPick); //Middle button is the answer
+            buttonText_2.text = answer.ToString();
+
+            Answers[0].onClick.AddListener(IncorrectPick);
+            buttonText_1.text = (answer + UnityEngine.Random.Range(1, 3)).ToString();
+
+            Answers[2].onClick.AddListener(IncorrectPick);
+            buttonText_3.text = (answer + UnityEngine.Random.Range(4, 5)).ToString();
+        }
+        else
+        {
+            Answers[correctPick].onClick.AddListener(CorrectPick); // Right button is the answer
+            buttonText_3.text = answer.ToString();
+
+            Answers[1].onClick.AddListener(IncorrectPick);
+            buttonText_2.text = (answer + UnityEngine.Random.Range(1, 3)).ToString();
+
+            Answers[0].onClick.AddListener(IncorrectPick);
+            buttonText_1.text = (answer + UnityEngine.Random.Range(4, 5)).ToString();
+        }
+
+
+
+       
+
+
+
+    }
+
+    public void CorrectPick()
+    {
+
+        print("This is the answer button");
+
+
+
+        for(int i = 0; i < Answers.Length; i++)
+        {
+            Answers[i].onClick.RemoveListener(CorrectPick);
+        }
+        for (int i = 0; i < Answers.Length; i++)
+        {
+            Answers[i].onClick.RemoveListener(IncorrectPick);
+        }
+    }
+
+    public void IncorrectPick()
+    {
+        print("This is the incorrect answer button");
+        for (int i = 0; i < Answers.Length; i++)
+        {
+            Answers[i].onClick.RemoveListener(CorrectPick);
+        }
+
+        for (int i = 0; i < Answers.Length; i++)
+        {
+            Answers[i].onClick.RemoveListener(IncorrectPick);
+        }
+    }
+
+    // Start is called before the first frame update
+   
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
